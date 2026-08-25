4681a4a26b30b0e8fa1f3955515d0d32c15815ec
Den-R3R5 <deniskostin235@gmail.com>
2026-08-25 23:55:16 +1100

    Add index route with flash handler

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,26 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask
+from flask import (
+    Flask,
+    flash,
+    get_flashed_messages,
+    redirect,
+    render_template,
+    url_for,
+)
 
 load_dotenv()
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
+
+@app.get("/")
+def index():
+    messages = get_flashed_messages(with_categories=True)
+
+    return render_template(
+        "index.html",
+        messages=messages,
+    )
 
