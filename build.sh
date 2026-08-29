9a8da4808457d71617c8730d5ea95da1c9632a75
Den-R3R5 <deniskostin235@gmail.com>
2026-08-29 22:26:06 +1100

    New db init method 2

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
 uv run tailwindcss -i ./page_analyzer/static/src/input.css -o ./page_analyzer/static/css/style.css --minify
-
-uv run python -c "from page_analyzer.db import init_db; init_db()"
