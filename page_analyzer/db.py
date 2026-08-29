2c4d304d18bde5863136d8db0450c49842d2029f
Den-R3R5 <deniskostin235@gmail.com>
2026-08-29 15:40:33 +1100

    Add db handler

---
+++ page_analyzer/db.py
@@ -0,0 +1,56 @@
+import os
+
+import psycopg
+from dotenv import load_dotenv
+from psycopg.rows import namedtuple_row
+
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+def _connect():
+    return psycopg.connect(DATABASE_URL)
+
+
+def get_url_by_name(name):
+    with _connect() as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            cur.execute(
+                "SELECT * FROM urls WHERE name = %s;",
+                (name,),
+            )
+            result = cur.fetchone()
+    return result
+
+
+def get_url_by_id(id):
+    with _connect() as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            cur.execute(
+                "SELECT * FROM urls WHERE id = %s;",
+                (id,),
+            )
+            result = cur.fetchone()
+    return result
+
+
+def add_url(name):
+    with _connect() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "INSERT INTO urls(name) VALUES(%s) RETURNING id",
+                (name,),
+            )
+            id = cur.fetchone()[0]
+            conn.commit()
+    return id
+
+
+def get_all_urls():
+    with _connect() as conn:
+        with conn.cursor(row_factory=namedtuple_row) as cur:
+            cur.execute("SELECT * FROM urls;")
+            result = cur.fetchall()
+    return result
