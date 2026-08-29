686a22b109b6eb9489c9c4fd6ab00d58bbb43a50
Den-R3R5 <deniskostin235@gmail.com>
2026-08-29 15:41:01 +1100

    Add db & link handler

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -1,3 +1,13 @@
 from .app import app
+from .db import add_url, get_all_urls, get_url_by_id, get_url_by_name
+from .utils import link_normalize, link_validate
 
-__all__ = ("app",)
+__all__ = (
+    "app",
+    "add_url",
+    "get_url_by_name",
+    "get_url_by_id",
+    "get_all_urls",
+    "link_normalize",
+    "link_validate",
+)
