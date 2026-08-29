269e5ea85eb57361d8341bd277acc38b830070d7
Den-R3R5 <deniskostin235@gmail.com>
2026-08-29 15:34:51 +1100

    Add link validator

--- page_analyzer/utils.py
+++ page_analyzer/utils.py
@@ -1,7 +1,15 @@
 from urllib.parse import urlparse
 
+from validators import url
+
 
 def link_normalize(link):
     parsed_link = urlparse(link)
     normalized = f"{parsed_link.scheme}://{parsed_link.netloc}".lower()
     return normalized
+
+
+def link_validate(link):
+    if url(link) and len(link) <= 255:
+        return True
+    return False
