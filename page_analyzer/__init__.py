6a0ff03effe48eb520816cbf59a06904fd4aa04e
Den-R3R5 <deniskostin235@gmail.com>
2026-09-10 20:00:39 +1100

    Refactor

--- page_analyzer/__init__.py
+++ page_analyzer/__init__.py
@@ -1,6 +1,8 @@
 from .app import app
 from .db import (
+    add_check,
     add_url,
+    get_all_checks,
     get_all_urls,
     get_url_by_id,
     get_url_by_name,
@@ -10,7 +12,9 @@ from .utils import link_normalize, link_validate
 
 __all__ = (
     "app",
+    "add_check",
     "add_url",
+    "get_all_checks",
     "get_all_urls",
     "get_url_by_id",
     "get_url_by_name",
